bc66b2637f79643f6df0dfd7d932e8baebf9547a
Naveen <naveenmmedikurthi@gmail.com>
2023-07-26 00:12:49 +0530

    FrontEnd

--- FrontEnd.sh
+++ FrontEnd.sh
@@ -1,13 +1,15 @@
 echo installing nginx
 yum install nginx -y
 
-systemctl enable nginx
-systemctl start nginx
 
-cp NginxFrontEnd.conf /etc/nginx/default.d/roboshop.conf
+
+cp NginxFrontend.conf /etc/nginx/default.d/roboshop.conf
 echo Clearing Conetent
 rm -rf /usr/share/nginx/html/*
 Echo Downloading HTML Content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+systemctl enable nginx
+systemctl start nginx
+systemctl restart nginx
 
 
